b7c314743aa8940cdbcecc570e3948b4d1ef9584
Orfeas Antoniou <orfeas@orfeasa.com>
2023-12-03 22:52:50 +0100

    Improve efficiency of input parsing

--- day_03/main.py
+++ day_03/main.py
@@ -30,36 +30,39 @@ def part_one(filename: str) -> int:
 
 
 def part_two(filename: str) -> int:
+    symbol_coordinates, parts = parse_input(filename)
+    # find gears that are adjacent to exactly 2 part numbers
     return 0
 
 
-def parse_input(filename: str):
-    numbers_chars = {str(ch) for ch in range(0, 10)}
-    parts = set()
-
+def parse_input(filename: str) -> Tuple[Set[Tuple[int, int]], Set[Tuple[int, int]]]:
     with open(filename, encoding="utf8") as f:
         lines = [line.strip() for line in f.readlines()]
 
-    symbol_coordinates = set(
-        ch for line in lines for ch in line if ch not in numbers_chars.union({"."})
-    )
+    numbers_chars = {str(ch) for ch in range(0, 10)}
+    symbol_coordinates = {
+        (x, y)
+        for y, line in enumerate(lines)
+        for x, ch in enumerate(line)
+        if ch not in numbers_chars.union({"."})
+    }
+    parts = set()
     for y, line in enumerate(lines):
-        for x_start, ch in enumerate(line):
-            if line[x_start] in numbers_chars and (
-                x_start == 0 or line[x_start - 1] not in numbers_chars
-            ):
-                x_end = x_start
-                while line[x_end] in numbers_chars and x_end < len(line):
-                    x_end += 1
-                x_end -= 1
+        x = 0
+        while x < len(line):
+            if line[x] in numbers_chars:
+                x_start = x
+                while x + 1 < len(line) and line[x + 1] in numbers_chars:
+                    x += 1
                 parts.add(
                     PartNumber(
                         x_start=x_start,
-                        x_end=x_end,
+                        x_end=x,
                         y=y,
-                        value=int(line[x_start : x_end + 1]),
+                        value=int(line[x_start : x + 1]),
                     )
                 )
+            x += 1
 
     return symbol_coordinates, parts
 
