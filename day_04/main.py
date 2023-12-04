e7f4f140cbc555ceb8936a92b411dc032195b2ed
Orfeas Antoniou <orfeas@orfeasa.com>
2023-12-04 21:24:40 +0000

    Day 4 part 1

--- day_04/main.py
+++ day_04/main.py
@@ -1,13 +1,28 @@
 def part_one(filename: str) -> int:
-    return 0
+    scratchcards = parse_input(filename)
+    return sum(
+        2 ** (len(matches) - 1)
+        for winning, current in scratchcards.items()
+        if (matches := set(winning).intersection(current))
+    )
 
 
 def part_two(filename: str) -> int:
     return 0
 
 
+def parse_input(filename: str) -> dict[tuple, set]:
+    with open(filename, encoding="utf8") as file:
+        return {
+            tuple(sorted(map(int, line.split(":")[1].split("|")[0].split()))): set(
+                map(int, line.split("|")[1].split())
+            )
+            for line in file
+        }
+
+
 if __name__ == "__main__":
-    input_path = "./day_xx/input.txt"
+    input_path = "./day_04/input.txt"
     print("---Part One---")
     print(part_one(input_path))
 
